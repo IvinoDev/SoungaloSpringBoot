502dff81a285464ec2be176c32ae8b79b661fb61
IvinoDev <kayantao@protonmail.com>
2023-01-07 14:00:23 +0000

    initial commit

---
+++ src/test/java/ml/smartyiriba/soungalosb/SoungaloSbApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.smartyiriba.soungalosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoungaloSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
